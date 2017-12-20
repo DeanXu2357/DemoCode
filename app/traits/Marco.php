b02539b805d57b39ae70975912a5cf4f283e26d7
Dean Xu <dean.xu.2357@gmail.com>
2017-12-20 23:39:40 +0800

    範例程式--Marco ( 特徵版本

---
+++ app/traits/Marco.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\traits;
+
+use closure;
+
+trait Marco
+{
+    protected $marcos;
+    protected $data;
+
+    public function __construct()
+    {
+        $this->data = 'bar';
+    }
+
+    public function __call($name, $args)
+    {
+        if ($this->marcos[$name] instanceof closure) {
+            return $this->marcos[$name]->call($this, ...$args);
+        }
+    }
+
+    public function marco(string $name, closure $function)
+    {
+        if (isset($this->marcos[$name])) {
+            // TODO 回傳錯誤
+            throw new Exception('name is invalid');
+        } else {
+            $this->marcos[$name] = $function;
+        }
+    }
+}
